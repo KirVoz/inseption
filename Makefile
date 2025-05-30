a6eba38bb937fb5638e0d16d57c3cc2c985f2f57
KirVoz <k.a.voz97@gmail.com>
2025-05-30 17:42:37 +0400

    1.0(no bonus)

--- Makefile
+++ Makefile
@@ -1,9 +1,35 @@
-up:
-	docker compose -f srcs/docker-compose.yml up -d --build
+all: dirs start
+
+dirs:
+	mkdir -p "${HOME}/data/wp-data"
+	mkdir -p "${HOME}/data/db-data"
 
 down:
 	docker compose -f srcs/docker-compose.yml down
 
 d:
 	docker compose -f srcs/docker-compose.yml down --volumes
-	docker image prune -a -f
+	sudo rm -rf "${HOME}/data/wp-data"
+	sudo rm -rf "${HOME}/data/db-data"
+	docker system prune -a --volumes
+
+stop:
+	docker compose -f srcs/docker-compose.yml stop
+start:
+	docker compose -f srcs/docker-compose.yml up -d --build
+restart:
+	docker compose -f srcs/docker-compose.yml restart
+ps:
+	docker ps
+logs:
+	docker compose -f srcs/docker-compose.yml logs -fma
+
+db:
+	docker exec -it mariadb mariadb -ukvoznese -p123 wpdb
+	#SHOW DATABASES;
+	#USE wpdb;
+	#SHOW TABLES;
+	#SELECT * FROM wp_users;
+	#exit;
+
+.PHONY: init_dirs down d stop start restart ps logs db
--- srcs/requirements/wordpress/conf/create-wp-users.sh
+++ srcs/requirements/wordpress/conf/create-wp-users.sh
@@ -2,15 +2,23 @@
 
 cd /var/www/html
 
-until wp core is-installed --allow-root; do
+until mysqladmin ping -h"$WORDPRESS_DB_HOST" -u"$WORDPRESS_DB_USER" -p"$WORDPRESS_DB_PASSWORD" --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+
+if ! wp core is-installed --allow-root; then
     wp core install --url="${WP_SITE_URL}" \
         --title="${WP_SITE_TITLE}" \
         --admin_user="${WP_ADMIN_USER}" \
         --admin_password="${WP_ADMIN_PASSWORD}" \
         --admin_email="${WP_ADMIN_EMAIL}" \
         --skip-email \
-        --allow-root && break
-    sleep 5
-done
+        --allow-root
+fi
 
-wp user create "${WP_USER2_NAME}" "${WP_USER2_EMAIL}" --role=editor --user_pass="${WP_USER2_PASSWORD}" --allow-root
+if ! wp user get "${WP_USER2_NAME}" --field=user_login --allow-root > /dev/null 2>&1; then
+    wp user create "${WP_USER2_NAME}" "${WP_USER2_EMAIL}" --role=editor --user_pass="${WP_USER2_PASSWORD}" --allow-root
+fi
+
+php-fpm81 -F
